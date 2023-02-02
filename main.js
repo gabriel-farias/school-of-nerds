92786f38ed7b5885e028f49892fe789940c7270e
Gabriel Farias <gabrielyoung87@gmail.com>
2023-02-02 19:56:19 -0300

    se agrego funcion de filtrado por categorias en seccion de cursos

--- main.js
+++ main.js
@@ -10,11 +10,11 @@ const agregarAlCarrito = (curso) => {
   total += curso.precio_de_curso;
   const listaCursosHtml = carritoCursos
     .map((curso) => {
-      return `<li class="carrito__curso"> 
-        <img src="${curso.logo}" class="carrito__curso-logo" alt="curso logo"> 
-        <h6 class="carrito__curso-nombre">${curso.nombre_de_curso}</h6> 
-        <p class="carrito__curso-precio">$ ${curso.precio_de_curso}</p>
-        <button class="btn-eliminar" data-id="${curso.id}">Eliminar</button> 
+      return `<li class="carrito__curso container text-center row"> 
+        <img src="${curso.logo}" class="carrito__curso-logo col-2" alt="curso logo"> 
+        <h6 class="carrito__curso-nombre col-6">${curso.nombre_de_curso}</h6> 
+        <p class="carrito__curso-precio col-3">$ ${curso.precio_de_curso}</p>
+        <a class="col-1 " ><img src="./assets/delete btn.svg" class="btn-eliminar " data-id="${curso.id}" alt=""></a> 
       </li>`;
     })
     .join("");
@@ -26,22 +26,22 @@ const agregarAlCarrito = (curso) => {
 
 const agregarCursoHtml = (cursos) => {
   return `
-  <div class="card text-bg-dark" style="width: 24rem">
-  <img
-    src="${cursos.logo}"
-    class="card-img-top product__logo"
-    alt="html + css logo"
-    style="height: 45%"
-  />
-  <div class="card-body d-flex justify-content-evenly">
-    <h5 class="card-title product__name">${cursos.nombre_de_curso}</h5>
-    <p class="card-text fw-semibold fs-6">
-      ${cursos.detalle_de_curso}
-    </p>
-    <h5 class="card-price ">Precio: <span class="badge fs-5 product__price" >$ ${cursos.precio_de_curso}</span></h5>
-    <button class="btn btn-primary btn-add ">Agregar al carrito</button>
+  <div class="card text-bg-dark curso-cat" style="width: 24rem" data-category="${cursos.categoria}" >
+    <img
+      src="${cursos.logo}"
+      class="card-img-top product__logo"
+      alt="html + css logo"
+      style="height: 45%"
+    />
+    <div class="card-body d-flex justify-content-evenly"  >
+      <h5 class="card-title product__name">${cursos.nombre_de_curso}</h5>
+      <p class="card-text fw-semibold fs-6">
+        ${cursos.detalle_de_curso}
+      </p>
+      <h5 class="card-price ">Precio: <span class="badge fs-5 product__price" >$ ${cursos.precio_de_curso}</span></h5>
+      <button class="btn btn-primary btn-add ">Agregar al carrito</button>
+    </div>
   </div>
-</div>
 `;
 };
 const cursosHtml = cursos.map((curso) => agregarCursoHtml(curso)).join("");
@@ -69,7 +69,7 @@ carrito.addEventListener("click", (e) => {
     total -= curso.precio_de_curso;
     const listaCursosHtml = carritoCursos
       .map((curso) => {
-        return `<li class="carrito__curso"> <img src="${curso.logo}" class="carrito__curso-logo" alt="curso logo"> <h6 class="carrito__curso-nombre">${curso.nombre_de_curso}</h6> <p class="carrito__curso-precio">$ ${curso.precio_de_curso}</p> <button class="btn-eliminar" data-id="${curso.id}">Eliminar</button> </li>`;
+        return `<li class="carrito__curso container text-center row"> <img src="${curso.logo}" class="carrito__curso-logo col-2" alt="curso logo"> <h6 class="carrito__curso-nombre col-6">${curso.nombre_de_curso}</h6> <p class="carrito__curso-precio col-3">$ ${curso.precio_de_curso}</p> <a class="col-1" ><img src="./assets/delete btn.svg" class="btn-eliminar " data-id="${curso.id}"  alt=""></a> </li>`;
       })
       .join("");
     carrito.innerHTML = `
@@ -78,4 +78,29 @@ carrito.addEventListener("click", (e) => {
       <p>Precio total: $ ${total}</p>`;
     e.target.parentElement.remove();
   }
+});
+const buttonsFilt = document.querySelectorAll("button[data-filter]");
+const cursosFilt = document.querySelectorAll(".curso-cat");
+
+buttonsFilt.forEach((button) => {
+  button.addEventListener("click", () => {
+    const filter = button.getAttribute("data-filter");
+    if (filter === "all") {
+      cursosFilt.forEach((product) => {
+        product.style.display = "flex";
+        product.style.flexDirection = "column";
+        product.style.justifyContent = "space-evenly";
+      });
+    } else {
+      cursosFilt.forEach((product) => {
+        if (product.getAttribute("data-category") === filter) {
+          product.style.display = "flex";
+          product.style.flexDirection = "column";
+          product.style.justifyContent = "space-evenly";
+        } else {
+          product.style.display = "none";
+        }
+      });
+    }
+  });
 });
